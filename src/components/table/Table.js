69dd6d3e1167133fc18bceda4f10ce9ea3500cdc
vladzyabs <vladzyaba@gmail.com>
2020-07-08 19:17:53 +0300

    single cell selection added

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,7 +1,9 @@
 import {ExcelComponent} from '@core/ExcelComponent';
 import {creatTable} from '@/components/table/table.templat';
 import {resizeHandler} from '@/components/table/table.resize';
-import {shouldResize} from '@/components/table/table.function';
+import {isCell, shouldResize} from '@/components/table/table.function';
+import {TableSelection} from '@/components/table/TableSelection';
+import {$} from '@core/dom';
 
 export class Table extends ExcelComponent {
     static className = 'excel__table'
@@ -16,9 +18,22 @@ export class Table extends ExcelComponent {
         return creatTable()
     }
 
+    prepare() {
+        this.slection = new TableSelection()
+    }
+
+    init() {
+        super.init()
+        const $cell = this.$root.find('[data-id="0:0"]')
+        this.slection.select($cell)
+    }
+
     onMousedown(event) {
         if (shouldResize(event)) {
             resizeHandler(this.$root, event)
+        } else if (isCell(event)) {
+            const $target = $(event.target)
+            this.slection.select($target)
         }
     }
 }
